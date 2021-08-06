19f5850abe88135639709e466150c3fe0aa79354
Marhama <tsjazil@gmail.com>
2021-08-06 06:36:31 +0530

    adding .bashrc

--- i3blocks/cpu/cpu_info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TEMP=$(sensors | grep 'Package id 0:\|Tdie' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
-CPU_USAGE=$(sar -u 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
-echo "$CPU_USAGE $TEMP" | awk '{ printf "%04.1f", (100 - $8), $1, $2 }'
--- i3blocks/wifi/battery_info.sh
+++
@@ -1,1 +0,0 @@
-command=iwgetid -r | tr '\n' ' ' && grep $(iwgetid -m | awk '{ printf "%s", $1 }') /proc/net/wireless | awk '{ printf " %i%\n", int($3 * 100 / 70) }'
